4cb1b2e406a511e0a8fef9b2e8c921a276d252ac
D-H-O-R-A <diegoantunes2301@gmail.com>
2022-10-21 00:02:36 -0300

    tiktok

--- getdata.py
+++ getdata.py
@@ -2,7 +2,7 @@
 
 #Details about:
 
-# Before starting install TikTokApi using "pip install TikTokApi" and playwright using "python3 -m playwright install". Your system must have the dependencies installed "libicui18n.so.66 libicuuc.so.66 libwebp.so.6 libffi.so.7"
+# Before starting install TikTokApi using "pip install TikTokApi", pandas using "pip install pandas" and playwright using "python3 -m playwright install". Your system must have the dependencies installed "libicui18n.so.66 libicuuc.so.66 libwebp.so.6 libffi.so.7"
 
 # Solution presented:
 # Create a bot that enters the browser, enters the account you want to analyze and obtain the data by reading the content of the website.
@@ -13,40 +13,42 @@
 #Importing tiktok library
 from TikTokApi import TikTokApi
 import os
+import pandas as pd
 
-#asking the user to enter the Username of the account you want to be analyzed in tiktok
-account = str(input("Enter the Username of the account you want to be analyzed:"))
 
-#asking the user to enter the amount of videos he wants to analyze from the last video released by the account
-n = int(input("Enter the amount of videos you want to be analyzed from the last video posted:"))
+url = str(input("Video URL:"))
 
-#asking the user to enter the Proxie (It's needed because of captch) - https://github.com/davidteather/TikTok-Api/issues/397
-# proxy = str(input("Proxie (It's needed because of captch) (Ex: 80.48.119.28:8080 ):"))
 
-api = TikTokApi(custom_verify_fp=os.environ.get("verifyFp", None))
+# for i in data['urls']:
+#     print(i)
 
-user_id = api.user(username=account).info().id
-sec_uid = api.user(username=account).info().secUid
-get_videos = api.user(user_id=user_id, sec_uid=sec_uid).videos(count=n)
+def video_info(url):
+    with TikTokApi(custom_verify_fp=os.environ.get("verifyFp", None)) as api:
+        video = api.video(url=url)
+        return video.id
 
-#Defining a function with values ​​present in the TikTokApi request
-def list_data(snapshot):
-  listData = {}
-  listData['user_name'] = snapshot['author']['uniqueId']
-  listData['video_id'] = snapshot['id']
-  listData['video_desc'] = snapshot['desc']
-  listData['video_time'] = snapshot['createTime']
-  listData['video_length'] = snapshot['video']['duration']
-  listData['video_link'] = 'https://www.tiktok.com/@{}/video/{}?lang=en'.format(listData['user_name'], listData['video_id'])
-  listData['n_likes'] = snapshot['stats']['diggCount']
-  listData['n_shares'] = snapshot['stats']['shareCount']
-  listData['n_comments'] = snapshot['stats']['commentCount']
-  listData['n_plays'] = snapshot['stats']['playCount']
-  return listData
 
-#variable with details about the videos requested by the user
-videos_data = [list_data(n) for n in get_videos]
+def video_data(video_id):
+    with TikTokApi(custom_verify_fp=os.environ.get("verifyFp", None)) as api:
+        video = api.video(id=video_id)
+        data = video.info()
+        return data
 
-#Creating a spreadsheet with the data obtained.
-videos_data_ft = pd.DataFrame(videos_data)
-videos_data_ft.to_csv('{}_data_videos.csv'.format(username),index=False)
+def simple_dict(tiktok_dict):
+  to_return = {}
+  to_return['user_name'] = tiktok_dict['author']['uniqueId']
+  to_return['user_id'] = tiktok_dict['author']['id']
+  to_return['video_id'] = tiktok_dict['id']
+  to_return['video_desc'] = tiktok_dict['desc']
+  to_return['video_time'] = tiktok_dict['createTime']
+  to_return['video_length'] = tiktok_dict['video']['duration']
+  to_return['video_link'] = 'https://www.tiktok.com/@{}/video/{}?lang=en'.format(to_return['user_name'], to_return['video_id'])
+  to_return['n_likes'] = tiktok_dict['stats']['diggCount']
+  to_return['n_shares'] = tiktok_dict['stats']['shareCount']
+  to_return['n_comments'] = tiktok_dict['stats']['commentCount']
+  return to_return
+
+link_data = simple_dict(video_data(video_info(url)))
+print(link_data)
+user_videos_df = pd.DataFrame(link_data, index=[0])
+user_videos_df.to_csv('{}_videos.csv'.format(link_data['user_name']),index=False)
